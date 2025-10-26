993d08cbf1c018cc7524109db71e755f66be7d53
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-10-26 23:41:32 +0000

    enum and char

--- ReadingInputs/dataTypes.c
+++ ReadingInputs/dataTypes.c
@@ -5,9 +5,19 @@ int main()
 {
     // Data types
     float myFloat = 23.333;
+    
     double myDouble = 55.44444444444445e+11;
+    
     bool myBoolVariable = true;
+    
     int age = 30; 
+    
+    enum weekdays {Monday, Tuesday, Wednesday, Thursday, Friday};
+    
+    char myChar = 'A';
+    
+    char myString = "Hello !";
+    
     
     
     return 0;
